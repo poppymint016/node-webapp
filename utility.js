a6b2105ed60dd95e55872f7e5a5d836fee26f809
poppymint016 <phannita016@gmail.com>
2022-08-16 17:05:42 +0700

    add data

--- utility.js
+++ utility.js
@@ -1,6 +1,6 @@
 const request = require("request-promise");
 const EXTERNAL_API="https://tools.ecpe.nu.ac.th/network/api/student/"; // put url
-const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYwNTUyNTY2LCJleHAiOjE2NjA1NTYxNjYsIm5iZiI6MTY2MDU1MjU2NiwianRpIjoicHJjTFBFNXNiYXlQTnFUdyIsInN1YiI6NDgsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.DNWvZqC3epcfkWT6gO-sPIIwSxxgApXVvB4vp8zLa8g"; // put access token
+const accessToken="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC90b29scy5lY3BlLm51LmFjLnRoXC9uZXR3b3JrXC9hcGlcL2xvZ2luIiwiaWF0IjoxNjYwNjQzMjEwLCJleHAiOjE2NjA2NDY4MTAsIm5iZiI6MTY2MDY0MzIxMCwianRpIjoiTlRaWGhmQkhjNkRVMkN6bCIsInN1YiI6NDgsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.ATfqEqQ4JFF_uiDwPGeFZwvSZWzpNko3lYsI7X77JC8"; // put access token
 const student = {
   student_id: 63362518,
   name: '', // replace with your full name.
